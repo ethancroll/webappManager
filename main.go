9f09cc697d8694c411354bdf364a31f6450a9ff9
ethanc <ethan.m.carroll@gmail.com>
2026-02-18 20:32:36 -0600

    commented how it works

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ import (
 )
 
 func main() {
-	go run("npm", "/home/ethan/localFile", "start")
-	run("python3", "/home/ethan/secretsHolder", "-m", "http.server", "9000")
+	go run("npm", "C:\\code\\localFile", "start")                          //example of running node server
+	run("python3", "C:\\code\\secretsHolder", "-m", "http.server", "9000") //example of running html (using python http.server)
 }
 
 func run(cmd, dir string, args ...string) {
-	c := exec.Command(cmd, args...)
-	c.Dir = dir
-	c.Stdout = os.Stdout
-	c.Stderr = os.Stderr
-	c.Run()
+	c := exec.Command(cmd, args...) //builds the command with the provided arguments
+	c.Dir = dir                     //sets the working directory for the command
+	c.Stdout = os.Stdout            //reroutes command output
+	c.Stderr = os.Stderr            //reroutes command errors
+	c.Run()                         //runs the command
 }
